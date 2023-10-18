a640c7f2db5a0c00f545c0fbbce16296d46a0caf
batuhaniskr <batuhaniskr@gmail.com>
2023-10-18 11:27:07 +0300

    added vulnerable config

---
+++ src/test/java/com/batuhaniskr/vulnerablewebapp/XmlServiceTests.java
@@ -0,0 +1,15 @@
+package com.batuhaniskr.vulnerablewebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpHeaders;
+
+import javax.sql.DataSource;
+
+public class XmlServiceTests {
+
+    @Test
+    public void xmlTest() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "lqqq1231231");
+    }
+}
